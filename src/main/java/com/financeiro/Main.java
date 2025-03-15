de955bfbf21a0793e00a6e889835ca05f21040a4
FelipeOropeza <felipe2006.co@gmail.com>
2025-03-14 22:25:18 -0300

    feat: usando model e importação de class em java

--- src/main/java/com/financeiro/Main.java
+++ src/main/java/com/financeiro/Main.java
@@ -1,7 +1,11 @@
 package com.financeiro;
+import com.financeiro.model.Contas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Contas contas = new Contas(2, "Conta de luz", 200.00, false);
+        contas.setId(5);
+
+        System.out.println(contas.getId()+" "+contas.getNome()+" "+contas.getValor()+" "+contas.getTipoConta());
     }
 }
