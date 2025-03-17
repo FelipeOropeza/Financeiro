983c1aef4841c250ea28403ad6a76be3b20c506f
FelipeOropeza <felipe2006.co@gmail.com>
2025-03-17 00:14:01 -0300

    feat: trabalhando com telas com o swing

--- src/main/java/com/financeiro/Main.java
+++ src/main/java/com/financeiro/Main.java
@@ -1,11 +1,38 @@
 package com.financeiro;
-import com.financeiro.model.Contas;
 
-public class Main {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    public Main() {
+        setTitle("Menu Principal");
+        setLayout(null);
+        setSize(415, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton btnSair = new JButton("Sair");
+        btnSair.setBounds(200, 0, 200, 30);
+        add(btnSair);
+
+        btnSair.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        setLocationRelativeTo(null);
+    }
+
     public static void main(String[] args) {
-        Contas contas = new Contas(2, "Conta de luz", 200.00, false);
-        contas.setId(5);
-
-        System.out.println(contas.getId()+" "+contas.getNome()+" "+contas.getValor()+" "+contas.getTipoConta());
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main menu = new Main();
+                menu.setVisible(true);
+            }
+        });
     }
 }
