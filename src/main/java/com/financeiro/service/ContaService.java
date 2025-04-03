89e0c276e81330ef7919460ffc6de99e30d6fc95
FelipeOropeza <felipe2006.co@gmail.com>
2025-04-02 21:31:42 -0300

    feat: funcao pra listar as contas do banco de dados

--- src/main/java/com/financeiro/service/ContaService.java
+++ src/main/java/com/financeiro/service/ContaService.java
@@ -2,7 +2,10 @@ package com.financeiro.service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.financeiro.config.Conexao;
 import com.financeiro.model.Contas;
@@ -23,8 +26,27 @@ public class ContaService {
                 return true;
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("Erro ao buscar usuarios: " + e.getMessage());
             return false;
         }
+    }
+
+    public static List<Contas> selectContas(){
+        String sql = "SELECT * from contas";
+        List<Contas> contas = new ArrayList<>();
+
+        try (Connection connection = Conexao.connect();
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()){
+
+            while (rs.next()) {
+                Contas conta = new Contas(rs.getInt("id"), rs.getString("nome"), rs.getDouble("valor"), rs.getBoolean("tipoconta"));
+                contas.add(conta);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao buscar usuarios: " + e.getMessage());
+        }
+
+        return contas;
     }
 }
