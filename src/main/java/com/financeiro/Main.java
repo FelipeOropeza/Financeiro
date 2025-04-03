885a58ace4848bb296373b617b34b3c65ba2e5c2
FelipeOropeza <felipe2006.co@gmail.com>
2025-04-02 21:32:03 -0300

    feat: listagem das contas

--- src/main/java/com/financeiro/Main.java
+++ src/main/java/com/financeiro/Main.java
@@ -1,27 +1,34 @@
 package com.financeiro;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.SwingUtilities;
 
+import com.financeiro.model.Contas;
 import com.financeiro.screen.FormCad;
+import com.financeiro.service.ContaService;
 
 public class Main extends JFrame {
+
+    private final JList<String> listaContas;
 
     public Main() {
         setTitle("Menu Principal");
         setLayout(null);
-        setSize(415, 400);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JButton btnCadastro = new JButton("Cadastro");
-        btnCadastro.setBounds(0, 0, 200, 30);
+        btnCadastro.setBounds(0, 0, 250, 30);
         add(btnCadastro);
 
         JButton btnSair = new JButton("Sair");
-        btnSair.setBounds(200, 0, 200, 30);
+        btnSair.setBounds(250, 0, 250, 30);
         add(btnSair);
 
         btnSair.addActionListener((ActionEvent e) -> {
@@ -32,6 +39,19 @@ public class Main extends JFrame {
             new FormCad().setVisible(true);
             dispose();
         });
+
+        List<Contas> contas = ContaService.selectContas();
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (Contas conta : contas) {
+            String contaInfo = String.format("ID: %d - Nome: %s - Valor: R$ %.2f - Tipo: %s",
+                                             conta.getId(), conta.getNome(), conta.getValor(),
+                                             conta.getTipoConta() ? "Entrada" : "Saída");
+            model.addElement(contaInfo);
+        }
+
+        listaContas = new JList<>(model);
+        listaContas.setBounds(0, 50, 500, 200);
+        add(listaContas);
 
         setLocationRelativeTo(null);
     }
